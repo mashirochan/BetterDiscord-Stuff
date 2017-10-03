5500e0fbc7d04356b4994f0bd83ce7500538b775
mashirochan <kylebarsh@gmail.com>
2017-10-02 21:01:00 -0400

    Format and trim code

--- Plugins/FixVersionSpacing/FixVersionSpacing.plugin.js
+++ Plugins/FixVersionSpacing/FixVersionSpacing.plugin.js
@@ -13,22 +13,20 @@ var FixVersionSpacing = (function() {
 		load() {}
 
 		start() {
-			$('.button-1aU9q1').on('click.FixVersionSpacing', this.addSpaces);
+			$('.button-1aU9q1').on('click.FixVersionSpacing', addSpaces);
 			console.log(this.getName() + ' loaded. Current version: ' + this.getVersion());
 		}
-
-		unload() {}
 		
 		stop() {
 			$("*").off(".FixVersionSpacing");
 		}
-		
-		addSpaces() {
-			setTimeout(function() {
-				let e = document.querySelector('#bd-settings-sidebar > span > span');
-				e.innerHTML = e.innerHTML.replace('by', ' by ');
-			}, 500);
-		}
+	}
+	
+	function addSpaces() {
+		setTimeout(function() {
+			let e = document.querySelector('#bd-settings-sidebar > span > span');
+			e.innerHTML = e.innerHTML.replace('by', ' by ');
+		}, 500);
 	}
 	
 	return FixVersionSpacing;
