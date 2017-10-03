68ba83fc19f8f91de9575b859f97041666d64e29
mashirochan <kylebarsh@gmail.com>
2017-10-03 03:11:48 -0400

    Update FixVersionSpacing.plugin.js

--- Plugins/FixVersionSpacing/FixVersionSpacing.plugin.js
+++ Plugins/FixVersionSpacing/FixVersionSpacing.plugin.js
@@ -8,68 +8,68 @@ var FixVersionSpacing = (function() {
 
 		getAuthor() { return "Mashiro-chan"; }
 
-		getVersion() { return "1.0.2"; }
+		getVersion() { return "1.0.1"; }
 
 		load() {
-			checkForUpdate();
+			this.checkForUpdate();
 		}
 
 		start() {
-			$('.button-1aU9q1').on('click.FixVersionSpacing', addSpaces);
+			$('.button-1aU9q1').on('click.FixVersionSpacing', this.addSpaces);
 			console.log(this.getName() + ' loaded. Current version: ' + this.getVersion());
-			checkForUpdate();
+			this.checkForUpdate();
 		}
 		
 		stop() {
 			$("*").off(".FixVersionSpacing");
+		}	
+		
+		addSpaces() {
+			setTimeout(function() {
+				let e = document.querySelector('#bd-settings-sidebar > span > span');
+				e.innerHTML = e.innerHTML.replace('by', ' by ');
+			}, 500);
 		}
-	}
 	
-	function addSpaces() {
-		setTimeout(function() {
-			let e = document.querySelector('#bd-settings-sidebar > span > span');
-			e.innerHTML = e.innerHTML.replace('by', ' by ');
-		}, 500);
-	}
-	
-	function checkForUpdate() {
-		const githubRaw = "https://raw.githubusercontent.com/mashirochan/Mashiro-chan/master/Plugins/" + getName() + "/" + getName() + ".plugin.js";
-		$.get(githubRaw, (result) => {
-			var ver = result.match(/"[0-9]+\.[0-9]+\.[0-9]+"/i);
-			if (!ver) return;
-			ver = ver.toString().replace(/"/g, "");
-			this.remoteVersion = ver;
-			ver = ver.split(".");
-			var lver = this.getVersion().split(".");
-			if (ver[0] > lver[0]) this.hasUpdate = true;
-			else if (ver[0] == lver[0] && ver[1] > lver[1]) this.hasUpdate = true;
-			else if (ver[0] == lver[0] && ver[1] == lver[1] && ver[2] > lver[2]) this.hasUpdate = true;
-			else this.hasUpdate = false;
-			if (this.hasUpdate) {
-				this.showUpdateNotice();
-			}
-		});
-	}
+		checkForUpdate() {
+			const githubRaw = "https://raw.githubusercontent.com/mashirochan/Mashiro-chan/master/Plugins/" + this.getName() + "/" + this.getName() + ".plugin.js";
+			$.get(githubRaw, (result) => {
+				var ver = result.match(/"[0-9]+\.[0-9]+\.[0-9]+"/i);
+				if (!ver) return;
+				ver = ver.toString().replace(/"/g, "");
+				this.remoteVersion = ver;
+				ver = ver.split(".");
+				var lver = this.getVersion().split(".");
+				if (ver[0] > lver[0]) this.hasUpdate = true;
+				else if (ver[0] == lver[0] && ver[1] > lver[1]) this.hasUpdate = true;
+				else if (ver[0] == lver[0] && ver[1] == lver[1] && ver[2] > lver[2]) this.hasUpdate = true;
+				else this.hasUpdate = false;
+				if (this.hasUpdate) {
+					this.showUpdateNotice();
+				}
+			});
+		}
 
-	function showUpdateNotice() {
-		const updateLink = "https://betterdiscord.net/ghdl?url=https://github.com/mashirochan/Mashiro-chan/tree/master/Plugins/" + getName() + "/" + getName() + ".plugin.js"
-		BdApi.clearCSS("pluginNoticeCSS")
-		BdApi.injectCSS("pluginNoticeCSS", "#pluginNotice span, #pluginNotice span a {-webkit-app-region: no-drag;color:#fff;} #pluginNotice span a:hover {text-decoration:underline;}")
-		let noticeElement = '<div class="notice notice-info" id="pluginNotice"><div class="notice-dismiss" id="pluginNoticeDismiss"></div>The following plugins have updates: &nbsp;<strong id="outdatedPlugins"></strong></div>'
-		if (!$('#pluginNotice').length)  {
-			$('.app.flex-vertical').children().first().before(noticeElement);
-			$('.win-buttons').addClass("win-buttons-notice")
-			$('#pluginNoticeDismiss').on('click', () => {
-				$('.win-buttons').animate({top: 0}, 400, "swing", () => {$('.win-buttons').css("top","").removeClass("win-buttons-notice")});
-				$('#pluginNotice').slideUp({complete: () => {$('#pluginNotice').remove()}});
-			})
-		}
-		let pluginNoticeID = getName() + '-notice';
-		let pluginNoticeElement = $('<span id="' + pluginNoticeID + '">');
-		pluginNoticeElement.html('<a href="' + updateLink + '" target="_blank">' + getName() + '</a>');
-		if (!$('#'+pluginNoticeID).length) {
-			if ($('#outdatedPlugins').children('span').length) pluginNoticeElement.html(', ' + pluginNoticeElement.html());
-			$('#outdatedPlugins').append(pluginNoticeElement);
+		showUpdateNotice() {
+			const updateLink = "https://betterdiscord.net/ghdl?url=https://github.com/mashirochan/Mashiro-chan/tree/master/Plugins/" + this.getName() + "/" + this.getName() + ".plugin.js"
+			BdApi.clearCSS("pluginNoticeCSS")
+			BdApi.injectCSS("pluginNoticeCSS", "#pluginNotice span, #pluginNotice span a {-webkit-app-region: no-drag;color:#fff;} #pluginNotice span a:hover {text-decoration:underline;}")
+			let noticeElement = '<div class="notice notice-info" id="pluginNotice"><div class="notice-dismiss" id="pluginNoticeDismiss"></div>The following plugins have updates: &nbsp;<strong id="outdatedPlugins"></strong></div>'
+			if (!$('#pluginNotice').length)  {
+				$('.app.flex-vertical').children().first().before(noticeElement);
+				$('.win-buttons').addClass("win-buttons-notice")
+				$('#pluginNoticeDismiss').on('click', () => {
+					$('.win-buttons').animate({ top: 0 }, 400, "swing", () => { $('.win-buttons').css("top","").removeClass("win-buttons-notice"); });
+					$('#pluginNotice').slideUp({ complete: () => { $('#pluginNotice').remove(); } });
+				})
+			}
+			let pluginNoticeID = this.getName() + '-notice';
+			let pluginNoticeElement = $('<span id="' + pluginNoticeID + '">');
+			pluginNoticeElement.html('<a href="' + updateLink + '" target="_blank">' + this.getName() + '</a>');
+			if (!$('#' + pluginNoticeID).length) {
+				if ($('#outdatedPlugins').children('span').length) pluginNoticeElement.html(', ' + pluginNoticeElement.html());
+				$('#outdatedPlugins').append(pluginNoticeElement);
+			}
 		}
 	}
 	
