33179a5e00e047b4971340e7d55b80c2c43afc53
mashirochan <kylebarsh@gmail.com>
2017-10-23 15:02:06 -0400

    Update SelectionDefinition.plugin.js

--- Plugins/SelectionDefinition/SelectionDefinition.plugin.js
+++ Plugins/SelectionDefinition/SelectionDefinition.plugin.js
@@ -10,7 +10,19 @@ var SelectionDefinition = (function() {
 			.sd-word {font-weight: bold}
 			.sd-pro {margin-left: 8px; font-size: 13px; color: rgba(255, 255, 255, 0.8) font-family: 'Roboto', arial, sans-serif}
 			.sd-def {display: block; font-size: 12px; color: rgba(255, 255, 255, 0.7)}
+			.sd-item {color: rgba(255, 255, 255, 0.6); margin: 2px 0px; padding: 6px 9px; line-height: 16px; font-size: 13px; font-weight: 500;}
+			.sd-item:hover {color: #FFF; background: rgba(0, 0, 0, 0.2);}
+			.sd-backdrop {background-color: rgba(0, 0, 0, 0.5); border-radius: 8px; display: flex; contain: layout; position: absolute; z-index: 3000; height: 100%; width: 100%}
+			.sd-modal {background-color: rgba(100, 100, 100, 1.0); border-radius: 8px; height: 75%; width:50%; position: relative; top: 50%; left: 50%; transform: translate(-50%,-50%);}
 			`;
+			
+			this.contextEntryMarkup =
+			`<div class="item-group">
+				<div class="sd-item">
+					<span>Get Definition</span>
+					<div class="hint"></div>
+				</div>
+			</div>`;
 		}
 
 		getName() { return "SelectionDefinition"; }
@@ -29,17 +41,78 @@ var SelectionDefinition = (function() {
 			this.clientRect = [];
 			this.oldSelectionText;
 			this.wordData = {};
+			this.timer = null;
+			this.seconds = 2;
 			
-			$('.app').append('<div class="sd-popup">');
+			$('#app-mount').append('<div class="sd-popup">');
 			$('.sd-popup').append('<span class="sd-word">').append('<span class="sd-pro">').append('<span class="sd-def">');
+			$('.sd-word').text('example');
+			$('.sd-pro').text('/iɡˈzampəl/');
+			$('.sd-def').html('noun · <i>"a thing characteristic of its kind or illustrating a general rule."</i>');
 			
-			$(document).on('mouseup.SelectionDefinition', () => { // dynamically wrap selection in span
-				let selection = window.getSelection();
-				let tempClientRect = selection.getRangeAt(0).getClientRects()[0];
-				if (selection.anchorNode != null && tempClientRect && tempClientRect.width != 0) this.clientRect = tempClientRect;
+			$('.sd-popup').hide();
+			
+			this.serverContextObserver = new MutationObserver((changes, _) => {
+				changes.forEach(
+					(change, i) => {
+						if (change.addedNodes) {
+							change.addedNodes.forEach((node) => {
+								if (node.nodeType == 1 && node.className.includes("context-menu")) {
+									this.onContextMenu(node);
+								}
+							});
+						}
+					}
+				);
+			});
+			if (document.querySelector(".app")) this.serverContextObserver.observe(document.querySelector(".app"), {childList: true});
+			
+			$('#app-mount').on('mouseup.SelectionDefinition', '.markup', (e) => {
+				console.log('mouseup: ' + e.which);
+				$('.sd-popup').hide();
+				
+				let message = e.target;
+				if (window.getSelection().anchorNode == null || e.which == 2) return;
+				if (e.which == 3) {
+					clearTimeout(this.timer);
+					this.timer = null;
+					return;
+				}
+				let selectRegion = window.getSelection().getRangeAt(0).getClientRects()[0];
+				$('.sd-highlight').remove();
+				if (selectRegion && selectRegion.width > 0) {
+					$(message).append('<span class="sd-highlight">');
+					$('.sd-highlight').css({height: selectRegion.height, width: selectRegion.width, left: selectRegion.left - 310, right: selectRegion.right, top: selectRegion.top - 55, bottom: selectRegion.bottom, position: "absolute", "background-color": "rgba(255, 98, 98, 0.3)"});
+				}
+				
+				if (!this.timer)
+				this.timer = setTimeout(() => {
+					if ($('.sd-highlight').length) $('.sd-popup').show().css({"left": $('.sd-highlight').offset().left, "top": $('.sd-highlight').offset().top - 40});
+					this.timer = null;
+				}, 1 * 1000);
 			});
 			
-			$(document).on('mousemove.SelectionDefinition', ev => this.showPopup(ev)); // add setting to change hover duration requirement
+			$('#app-mount').on('mouseenter.SelectionDefinition', '.sd-highlight', e => {
+				console.log('mouseenter');
+				if (!this.timer)
+				this.timer = setTimeout(() => {
+					$('.sd-popup').show().css({"left": $(e.target).offset().left, "top": $(e.target).offset().top - 40});
+					this.timer = null;
+				}, 1 * 1000);
+			});
+			
+			$('#app-mount').on('mouseleave.SelectionDefinition', '.sd-highlight', () => {
+				console.log('mouseleave');
+				clearTimeout(this.timer);
+				this.timer = null;
+				$('.sd-popup').hide();
+			});
+			
+			$('#app-mount').on('click.SelectionDefinition', '.sd-item', () => this.showModal());
+			
+			$('#app-mount').on('click.SelectionDefinition', '.sd-backdrop', () => {
+				$('.sd-backdrop').remove();
+			});
 			
 			BdApi.clearCSS(this.stylesheet_name);
 			BdApi.injectCSS(this.stylesheet_name, this.stylesheet);
@@ -48,31 +121,42 @@ var SelectionDefinition = (function() {
 		}
 
 		stop() {
-			$(document).off(".SelectionDefinition");
+			$('*').off(".SelectionDefinition");
 			$('[class^="sd-"]').remove();
 			BdApi.clearCSS(this.stylesheet_name);
 		}
-
-		showPopup(ev) {
-			$('.sd-popup').hide();
-			if (ev.pageX >= this.clientRect.left && ev.pageX <= this.clientRect.right && ev.pageY >= this.clientRect.top  && ev.pageY <= this.clientRect.bottom) {
-				let selectionText = this.getSelectionText();
-				if (selectionText == '' || /([^a-z ])+/ig.test(selectionText)) $('sd-popup').hide();
-				else {
-					if (selectionText != this.oldSelectionText) this.wordData = this.getWordData(selectionText);
-					this.oldSelectionText = selectionText;
-					
-					$('.sd-popup').show();
-					$('.sd-word').text(this.wordData.word);
-					$('.sd-pro').text('/' + this.wordData.pronunciation + '/');
-					$('.sd-def').html(this.wordData.type + ' · <i>"' + this.wordData.definition + '."</i>');
-					let newTop = this.clientRect.top - $('.sd-popup').outerHeight();
-					let newLeft = this.clientRect.left;
-					$('.sd-popup').css({"top": newTop, "left": newLeft});
-				}
+		
+		onContextMenu (context) {
+			if ($('.sd-highlight').length) {
+				$('.item-group > .sd-item').remove();
+				$(context).append(this.contextEntryMarkup);
 			}
 		}
 
+		showPopup() {
+			$('.sd-popup').hide();
+			let selectionText = this.getSelectionText();
+			if (selectionText == '' || /([^a-z ])+/ig.test(selectionText)) $('sd-popup').hide();
+			else {
+				if (selectionText != this.oldSelectionText) this.wordData = this.getWordData(selectionText);
+				this.oldSelectionText = selectionText;
+				
+				$('.sd-popup').show();
+				$('.sd-word').text(this.wordData.word);
+				$('.sd-pro').text('/' + this.wordData.pronunciation + '/');
+				$('.sd-def').html(this.wordData.type + ' · <i>"' + this.wordData.definition + '."</i>');
+				let newTop = this.clientRect.top - $('.sd-popup').outerHeight();
+				let newLeft = this.clientRect.left;
+				$('.sd-popup').css({"top": newTop, "left": newLeft});
+			}
+		}
+
+		showModal() {
+			$('.sd-backdrop').remove();
+			$('.app').append('<div class="sd-backdrop">');
+			$('.sd-backdrop').append('<div class="sd-modal">');
+		}
+		
 		getWordData(word) {
 			let url = 'od-api.oxforddictionaries.com:443/api/v1/entries/en/'+ word.replace(/ /g, '_') + '/regions=us';
 			let returnData = {
